5dbe6dbd9f1ba330b89fb69fcecf6418cfd6dd55
ZhurbaMax <maksimjurba90210@gmail.com>
2020-12-23 17:47:24 +0200

    first commit 2

---
+++ functions/animals.php
@@ -0,0 +1,19 @@
+<?php
+
+$monster = array("booooD" => array ("growth" => 35,
+    "weight" => 30,
+    "force" => 39
+),
+    "dirti_monster" => array ("growth" => 30,
+        "weight" => 32,
+        "force" => 29
+    ),
+    "zara_monster" => array ( "growth" => 31,
+        "weight" => 22,
+        "force" => 39
+    ),
+    "zenda_monster" => array ( "growth" => 20,
+        "weight" => 20,
+        "force" => 20
+    )
+);
---
+++ functions/logic.php
@@ -0,0 +1,6 @@
+<?php
+require_once ("animals.php");
+function name(){
+    var_dump($monster);
+
+}
